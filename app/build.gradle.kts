4adb97cc49da3a629e92cd9971000b8b624ce1e8
Ema Javor <emajavor@Emas-MacBook-Pro.local>
2025-06-26 14:57:06 +0200

    success path unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,6 @@ dependencies {
 
 
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
